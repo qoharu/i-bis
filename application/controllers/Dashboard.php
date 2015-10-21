ff874fadceba9ab9f1046b1c46ca45e2c03b14bc
harkce <habibgarut@gmail.com>
2015-10-22 04:26:15 +0700

    Nambah lihat rekam medis

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,12 +5,20 @@
 			$this->load->view('navbar');
 			$this->load->view('sidebar');
 			$this->load->view('content');
+			$this->load->view('footer');
 		}
 
 		function input() {
 			$this->load->view('navbar');
 			$this->load->view('sidebar');
 			$this->load->view('input');
+			$this->load->view('footer');
+		}
+
+		function lihat() {
+			$this->load->view('navbar');
+			$this->load->view('sidebar');
+			$this->load->view('lihat');
 			$this->load->view('footer');
 		}
 
---
+++ application/views/lihat.php
@@ -0,0 +1,55 @@
+                <div class="span9">
+                    <div class="content">
+
+                        <div class="module">
+                            <div class="module-head">
+                                <h3>Data Rekam Medis</h3>
+                            </div>
+                            <div class="module-body table">
+                                <table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped  display" width="100%">
+                                    <thead>
+                                      <tr>
+                                        <th>Nomor BPJS Peserta</th>
+                                        <th>Nama Peserta</th>
+                                        <th>Diagnosa</th>
+                                        <th>Tindakan</th>
+                                        <th>Keterangan</th>
+                                        <th>Dokter</th>
+                                      </tr>
+                                    </thead>
+                                    <tbody>
+                                      <tr>
+                                        <th>12345</th>
+                                        <th>Bunglon Ganteng</th>
+                                        <th>Sehat</th>
+                                        <th>Ngoding</th>
+                                        <th>i-Bis</th>
+                                        <th>Cinta</th>
+                                      </tr>
+                                      <tr>
+                                        <th>1301144118</th>
+                                        <th>Habib Fikri</th>
+                                        <th>Sehat</th>
+                                        <th>Terbang</th>
+                                        <th>Cingkleung</th>
+                                        <th>Spesialis komputer</th>
+                                      </tr>
+                                    </tbody>
+                                    <tfoot>
+                                      <tr>
+                                        <th>Nomor BPJS Peserta</th>
+                                        <th>Nama Peserta</th>
+                                        <th>Diagnosa</th>
+                                        <th>Tindakan</th>
+                                        <th>Keterangan</th>
+                                        <th>Dokter</th>
+                                      </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div><!--/.module-->
+
+                    <br />
+                        
+                    </div><!--/.content-->
+                </div><!--/.span9-->
