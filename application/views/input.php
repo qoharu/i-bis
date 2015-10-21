b101d444bbb1c63f1ecfbed4674f91e6a697d0dc
harkce <habibgarut@gmail.com>
2015-10-22 03:35:14 +0700

    Input rekam medis

--- application/views/input.php
+++ application/views/input.php
@@ -5,7 +5,45 @@
                                     <h3>
                                         Input Rekam Medis</h3>
                                 </div>
-                                
+                                <div class="module-body">
+                                    <form class="form-horizontal row-fluid">
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="nomorPesertaBPJS">Nomor BPJS peserta</label>
+                                            <div class="controls">
+                                                <input type="text" id="nomorPesertaBPJS" placeholder="Masukkan nomor BPJS peserta..." class="span8">
+                                            </div>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="diagnosa">Diagnosa</label>
+                                            <div class="controls">
+                                                <input type="text" id="diagnosa" placeholder="Masukkan kode diagnosa..." class="span8">
+                                            </div>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="tindakan">Tindakan</label>
+                                            <div class="controls">
+                                                <input type="text" id="tindakan" placeholder="Tindakan yang dilakukan" class="span8">
+                                            </div>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <label class="control-label" for="basicinput">Keterangan</label>
+                                            <div class="controls">
+                                                <textarea class="span8" rows="5"></textarea>
+                                            </div>
+                                        </div>
+
+                                        <div class="control-group">
+                                            <div class="controls">
+                                                <button type="submit" class="btn">Input Rekam Medis</button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+                                </div>
                             </div>
                             <!--/.module-->
                         </div>
