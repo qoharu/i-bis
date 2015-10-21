6b8d7773b14721dc4ef840d5b1b249bbf69db210
harkce <habibgarut@gmail.com>
2015-10-21 21:04:00 +0700

    Sidebar 1.0

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -8,30 +8,19 @@
                                 </a></li>
                                 <li><a href="#"><i class="menu-icon icon-table"></i>Lihat Rekam Medis</a>
                                 </li>
-                                <li><a href="#"><i class="menu-icon icon-user-plus"></i>Register User</a>
+                                <li><a href="#"><i class="menu-icon icon-plus"></i>Register User</a>
+                                </li>
+                                <li><a href="#"><i class="menu-icon icon-play"></i>Cek Status Pembayaran</a>
+                                </li>
+                                <li><a href="#"><i class="menu-icon icon-exchange"></i>Transaksi</a>
+                                </li>
+                                <li><a href="#"><i class="menu-icon icon-medkit"></i>Layanan dan Stok</a>
                                 </li>
                             </ul>
                             <!--/.widget-nav-->
-                            
-                            
+
                             <ul class="widget widget-menu unstyled">
-                                <li><a href="ui-button-icon.html"><i class="menu-icon icon-bold"></i> Buttons </a></li>
-                                <li><a href="ui-typography.html"><i class="menu-icon icon-book"></i>Typography </a></li>
-                                <li><a href="form.html"><i class="menu-icon icon-paste"></i>Forms </a></li>
-                                <li><a href="table.html"><i class="menu-icon icon-table"></i>Tables </a></li>
-                                <li><a href="charts.html"><i class="menu-icon icon-bar-chart"></i>Charts </a></li>
-                            </ul>
-                            <!--/.widget-nav-->
-                            <ul class="widget widget-menu unstyled">
-                                <li><a class="collapsed" data-toggle="collapse" href="#togglePages"><i class="menu-icon icon-cog">
-                                </i><i class="icon-chevron-down pull-right"></i><i class="icon-chevron-up pull-right">
-                                </i>More Pages </a>
-                                    <ul id="togglePages" class="collapse unstyled">
-                                        <li><a href="other-login.html"><i class="icon-inbox"></i>Login </a></li>
-                                        <li><a href="other-user-profile.html"><i class="icon-inbox"></i>Profile </a></li>
-                                        <li><a href="other-user-listing.html"><i class="icon-inbox"></i>All Users </a></li>
-                                    </ul>
-                                </li>
+                                <li><a href="#"><i class="menu-icon icon-cog"></i>Pengaturan</a></li>
                                 <li><a href="#"><i class="menu-icon icon-signout"></i>Logout </a></li>
                             </ul>
                         </div>
