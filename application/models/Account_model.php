f076a95d58ecb24fc59dd637b68bb2a192890acd
squhart <salman@labcnc.com>
2015-10-21 19:43:50 +0700

    cek

--- application/models/Account_model.php
+++ application/models/Account_model.php
@@ -6,7 +6,20 @@ class Account_model extends CI_Model
 {
 	
 
-
+	function validate($data){
+		$password=md5($data['password']);
+		$query="select * from user where email='$data[email]' and password='$password' ";
+		$db=$this->db->query($query);
+		if($db->num_rows()==1){
+			$data=$db->row();
+			$admin = false;
+			if ($data->email=='admin@smartid.com') $admin = true;
+			$this->setsession($data,$admin);
+			return true;
+		}else{
+			return false;
+		}
+}
 
 	
 }
